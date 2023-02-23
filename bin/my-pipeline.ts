c0792d196c70301e924e7dfd12ce002e564fcf03
Dale Orders <daorders@amazon.com>
2023-02-23 15:43:36 +1100

    initial commit

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { MyPipelineStack } from '../lib/my-pipeline-stack';
 
+
 const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {
   env: {
