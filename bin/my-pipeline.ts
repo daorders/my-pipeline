04266f0623aa18ba93cd11f61a7b159ac5cf4812
Dale Orders <daorders@amazon.com>
2023-02-23 16:29:51 +1100

    initial commit

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -5,10 +5,10 @@ import { MyPipelineStack } from '../lib/my-pipeline-stack';
 
 const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {
-  // env: {
-  //   account: '854645663183',
-  //   region: 'ap-southeast-2',
-  // }
+  env: {
+    account: '854645663183',
+    region: 'ap-southeast-2',
+  }
 });
 
 app.synth();
