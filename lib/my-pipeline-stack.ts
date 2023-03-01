007a7e29fc53e745b3fae7f5652fbdb007409df3
Dale Orders <daorders@amazon.com>
2023-03-01 16:05:39 +1100

    removed secrets manager

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
