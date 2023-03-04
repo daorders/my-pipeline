36f04fe6f1d051fb6c56ce8b6f44fdc9d117a0b5
Dale Orders <daorders@amazon.com>
2023-03-04 22:14:12 +1100

    final commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,7 +16,16 @@ export class MyPipelineStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
-  }
 
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: {region: "ap-southeast-2" }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    testingStage.addPost(new ShellStep("validate", {
+      commands: ['../test/my-pipeline.test.ts'],
+    }));
+  }
 }
 
